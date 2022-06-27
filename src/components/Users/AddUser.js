5e7553a75b9e6f4e5412b2e59dc2cb4d8d5ac7ff
sup3rcow <bullandrooster@gmail.com>
2022-06-27 16:31:28 +0200

    user input and card

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import style from "./AddUser.module.css";
+import Card from "../UI/Card";
+
+const AddUser = (props) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Card className={style.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
